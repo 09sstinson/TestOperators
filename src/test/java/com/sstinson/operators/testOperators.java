21b1c992ac38b4d0a7af34a351c6bf2c61391102
09sstinson <09sstinson@gmail.com>
2020-07-17 13:19:17 +0100

    Initial commit

---
+++ src/test/java/com/sstinson/operators/testOperators.java
@@ -0,0 +1,14 @@
+package com.sstinson.operators;
+
+import org.junit.Test;
+
+public class testOperators {
+
+    @Test
+    public void testBinaryShift(){
+        int x = -3;
+        int y = x>>>1;
+        int n = 0;
+        System.out.println(y);
+    }
+}
